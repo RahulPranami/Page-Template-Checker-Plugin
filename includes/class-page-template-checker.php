d456c913e18d02a4de6c539930892da94e946c36
Rahul Pranami <rahul.p@mail.biztechcs.com>
2024-02-21 17:50:43 +0530

    Removed Public Facing Plugin Code

--- includes/class-page-template-checker.php
+++ includes/class-page-template-checker.php
@@ -77,7 +77,6 @@ class Page_Template_Checker {
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 
 	}
 
@@ -116,12 +115,6 @@ class Page_Template_Checker {
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-page-template-checker-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-page-template-checker-public.php';
-
 		$this->loader = new Page_Template_Checker_Loader();
 
 	}
@@ -156,22 +149,6 @@ class Page_Template_Checker {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Page_Template_Checker_Public( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
